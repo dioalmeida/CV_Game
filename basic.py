dadda1ca55cffcda22b06c642bd0346861483994
Rodrigo Ferreira <rodrigo.nas.aulas@gmail.com>
2021-12-06 19:06:20 +0000

    added jumping and camera toggling

--- basic.py
+++ basic.py
@@ -10,7 +10,7 @@ from direct.task import Task
 from panda3d.core import (CardMaker, Geom, GeomNode, GeomTriangles,
                           GeomVertexData, GeomVertexFormat, GeomVertexWriter,
                           Light, LVector3, PerspectiveLens, Point3, Spotlight,
-                          TextNode, Texture, Vec3, Vec4, lookAt,ClockObject)
+                          TextNode, Texture, Vec3, Vec4, lookAt,ClockObject, Camera)
 
 
 class GameObject():
@@ -87,14 +87,27 @@ def makeSquare(x1, y1, z1, x2, y2, z2):
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        #setting up scene
         self.scene = self.loader.loadModel("models/environment")
         self.scene.reparentTo(self.render)
         self.scene.setScale(.25,.25,.25)
         self.scene.setPos(-8,42,-3)
-       
-        self.taskMgr.add(self.followCubeTask, "followCubeTask")  
-        self.updateTask = self.taskMgr.add(self.update, "update")  
-        self.taskMgr.add(self.advanceCube,"advanceCube")
+        
+        #setting up cameras
+        self.camState = True # true = behind, false = side
+        self.dr = base.camNode.getDisplayRegion(0)
+        self.dr.setActive(1) 
+        self.camBehind= Camera("camBehind")
+        self.camSide = Camera("camSide")
+        self.cameraBehind = self.render.attachNewNode(self.camBehind)
+        self.cameraSide = self.render.attachNewNode(self.camSide)
+        self.dr.setCamera(self.cameraBehind)
+        self.taskMgr.add(self.followCubeBehindTask, "followCubeBehindTask")
+        self.taskMgr.add(self.followCubeSideTask, "followCubeSideTask")  
+        
+        
+        #setting up cube and its movement
+        self.updateTask = self.taskMgr.add(self.update, "update")
         square0 = makeSquare(-1, -1, -1, 1, -1, 1)
         square1 = makeSquare(-1, 1, -1, 1, 1, 1)
         square2 = makeSquare(-1, 1, 1, 1, -1, 1)
@@ -111,38 +124,86 @@ class MyApp(ShowBase):
 
         self.cube = self.render.attachNewNode(snode)
         self.cube.setTwoSided(True)
+        self.taskMgr.add(self.advanceCube,"advanceCube")
+        self.taskMgr.add(self.jumpCube,"jumpCube")
+        #self.jumpSpeed=3 #testing
+        self.jumping=False
+        self.jumpSpeed=0
         
-        self.camera.setPos(0,-10,0)
         
+        #setting up interactions
         self.keyMap = {
             "left" : False,
             "right" : False            
         }
+
         self.accept("a", self.updateKeyMap, ["left", True])
         self.accept("a-up", self.updateKeyMap, ["left", False])
         self.accept("d", self.updateKeyMap, ["right", True])
         self.accept("d-up", self.updateKeyMap, ["right", False])
-        
+        self.accept("w", self.jumpPressed)
+        self.accept("s", self.toggleCamera,[self.camState])
 
     def updateKeyMap(self, controlName, controlState):
         self.keyMap[controlName] = controlState
         print (controlName, "set to", controlState)
 
-    def followCubeTask(self, task):
+    def toggleCamera(self,state):
+        if self.camState:
+            self.dr.setCamera(self.cameraSide)
+        else:
+            self.dr.setCamera(self.cameraBehind)
+        print("switched cams")
+        self.camState = not self.camState
+
+    def followCubeBehindTask(self, task):
 
         
 
-        self.camera.setPos(self.cube.getX(), self.cube.getY()-15, 3)
-        self.camera.lookAt(self.cube)
+        self.cameraBehind.setPos(self.cube.getX(), self.cube.getY()-15, 3)
+        self.cameraBehind.lookAt(self.cube)
         
 
     
 
         return Task.cont
 
+    def followCubeSideTask(self, task):
+
+        
+
+        self.cameraSide.setPos(self.cube.getX()+ 20, self.cube.getY()-5, 3)
+        self.cameraSide.lookAt(self.cube)
+        
+
+    
+
+        return Task.cont
+
+    def toggleCameraTask(self, task):
+        pass
+
     def advanceCube(self, task):
         self.cube.setPos(self.cube.getPos() + Vec3(0,0.1, 0))
         return task.cont
+
+    def jumpPressed(self):
+        if self.jumping == False:
+            self.jumping=True
+            self.jumpSpeed=13
+            
+
+    def jumpCube(self, task):
+        dt = ClockObject.getGlobalClock().dt
+        self.cube.setZ(self.cube.getZ()+self.jumpSpeed*dt)
+        if self.cube.getZ() > 0:
+            self.jumpSpeed = self.jumpSpeed - 9.8*dt*2 #faster 
+        if self.cube.getZ() < 0:
+            self.cube.setZ(0)
+            self.jumpSpeed = 0
+            self.jumping = False
+        return task.cont
+
 
     def update(self,task):
         dt = ClockObject.getGlobalClock().dt
@@ -151,6 +212,7 @@ class MyApp(ShowBase):
             self.cube.setPos(self.cube.getPos() + Vec3(-5.0*dt,0, 0))
         if self.keyMap["right"]:
             self.cube.setPos(self.cube.getPos() + Vec3(5*dt,0, 0))
+        
         return task.cont
 
 app = MyApp()
