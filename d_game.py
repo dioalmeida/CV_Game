2db50d3867f2cf1efe82929faea5a682f26bb20a
nubanaga <doalmeida94@gmail.com>
2021-12-15 18:57:44 +0000

    add models

--- d_game.py
+++ d_game.py
@@ -7,26 +7,39 @@ from direct.task import Task
 from panda3d.core import *
 from panda3d.ode import *
 from panda3d.physics import *
+import gltf
 
 PLAYER_TAG = "player-instance"
+MAP_TAG = "map-instance"
+TRAPS_TAG = "traps-instance"
 SPEED = 15
 JUMP_SPEED = 10
 
-
+CAMERA_DIST_X = 40
+CAMERA_DIST_Y = 50
 class Game(ShowBase):
 
     def __init__(self):
         super().__init__()
+        gltf.patch_loader(self.loader) #enable gltf reading
+
         # Variable declaration
         self.speed = SPEED
         self.jump = 0
         self.jump_speed = JUMP_SPEED
+
+        self.sm = None
+        self.ground = None
+        self.contacts = None
+        self.space = None
         # Setup collision detection & physics
         self.setup_collision_detection()
         self.setup_physics()
         self.setup_ode()
-        self.add_ground()
+        self.add_lighting()
 
+        self.add_ground()
+        self.add_map()
         self.add_player()
         # Setup game world and instances
         #self.cam.setPos(0, -100, 10)
@@ -39,12 +52,15 @@ class Game(ShowBase):
         self.cameraSide = self.render.attachNewNode(self.camSide)
         self.dr.setCamera(self.cameraBehind)
         self.taskMgr.add(self.followCubeBehindTask, "followCubeBehindTask")
-        self.taskMgr.add(self.followCubeSideTask, "followCubeSideTask")  
+        self.taskMgr.add(self.followCubeSideTask, "followCubeSideTask")
         self.taskMgr.add(self.advanceCube,"advanceCube")
+
+
+
 
         # Start tasks
         self.taskMgr.add(self.update_ode, "UpdateODE")
-        self.taskMgr.add(self.spin_cam_task, "spinCamTask")
+        # self.taskMgr.add(self.spin_cam_task, "spinCamTask")
 
         # register event handlers
         self.keymap = {"left":False,"right":False,"jump":False}
@@ -59,6 +75,11 @@ class Game(ShowBase):
         self.accept("space-up", self.__update_keymap,["jump",False])
 
         # self.accept("%s-up" % (w_button), self.stop_moving_forward)
+    def add_lighting(self):
+        self.alight = AmbientLight("alight")
+        self.alight.setColor((0.2, 0.2, 0.2, 1))
+        self.alnp = self.render.attachNewNode(self.alight)
+        self.render.setLight(self.alnp)
 
     def toggleCamera(self,state):
         if self.camState:
@@ -69,15 +90,21 @@ class Game(ShowBase):
         self.camState = not self.camState
 
     def advanceCube(self, task):
-        self.sm.setPos(self.sm.getPos() + Vec3(0,10, 0))
+        player = self.render.find(PLAYER_TAG)
+        body = player.getPythonTag("body")
+        pos = body.getPosition()
+        dt = globalClock.getDt()
+        pos.y += 10 * dt
+        body.setPosition(pos)
+
+        player.setPosQuat(body.getPosition(), Quat(body.getQuaternion()))
+        # self.sm.setPos(self.sm.getPos() + Vec3(0,10, 0))
         return task.cont
 
 
     def followCubeBehindTask(self, task):
 
-        
-
-        self.cameraBehind.setPos(self.sm.getX(), self.sm.getY()-15, 3)
+        self.cameraBehind.setPos(self.sm.getX(), self.sm.getY()-CAMERA_DIST_Y, 3)
         self.cameraBehind.lookAt(self.sm)
         
 
@@ -89,7 +116,7 @@ class Game(ShowBase):
 
         
 
-        self.cameraSide.setPos(self.sm.getX()+ 20, self.sm.getY()-5, 3)
+        self.cameraSide.setPos(self.sm.getX()+ CAMERA_DIST_X, self.sm.getY()-5, 3)
         self.cameraSide.lookAt(self.sm)
         
 
@@ -130,18 +157,40 @@ class Game(ShowBase):
     def add_ground(self):
         cm = CardMaker("ground")
 
-        # cm.setFrame(-500, 500, -500, 500)
-        cm.setFrame(-20, 20, -20, 20)
-        ground = render.attachNewNode(cm.generate())
-        ground.setColor(0.2, 0.4, 0.8)
-        ground.lookAt(0, 0, -1)
+        cm.setFrame(-500, 500, -500, 500)
+        # cm.setFrame(-20, 20, -20, 20)
+        self.ground = render.attachNewNode(cm.generate())
+        self.ground.setColor(0.2, 0.4, 0.8)
+        self.ground.lookAt(0, 0, -1)
         groundGeom = OdePlaneGeom(self.space, Vec4(0, 0, 1, 0))
 
+    def add_map(self):
+        self.map = self.loader.loadModel("assets/map.glb")
+        # self.map = self.render.attachNewNode(MAP_TAG)
+        self.map.setPos(0,6,1)
+        self.map.reparentTo(self.render)
+        self.traps = self.loader.loadModel("assets/traps.glb")
+        # self.map = self.render.attachNewNode(MAP_TAG)
+        self.traps.setPos(0, 6, 1)
+        self.traps.reparentTo(self.render)
+        # body = OdeBody(self.odeWorld)
+        # mass = OdeMass()
+        # # mass.setSphereTotal(10, 1)
+        # mass.setBoxTotal(2000, 1, 1, 1)
+        # body.setMass(mass)
+        # body.setPosition(self.map.getPos())
+        # geom = OdeSphereGeom(self.space, 1)
+        # geom.setBody(body)
+        # self.map.setPythonTag("body", body)
     def add_player(self):
+        # self.testr = self.loader.loadModel("assets/cube.egg")
+        # self.testr.setPos(5, 5, 2)
+        # self.testr.reparentTo(self.render)
+
         self.player = self.loader.loadModel("assets/cube.egg")
         self.sm = self.render.attachNewNode(PLAYER_TAG)
         # sm.setPos(random.uniform(-20, 20), random.uniform(-30, 30), random.uniform(10, 30))
-        self.sm.setPos(0,0, 1)
+        self.sm.setPos(0,0, 4)
         self.player.instanceTo(self.sm)
         body = OdeBody(self.odeWorld)
         mass = OdeMass()
@@ -181,11 +230,12 @@ class Game(ShowBase):
     def spin_cam_task(self, task):
         self.cameraRadius = 30.0
 
-        # angleDegrees = 1  # task.time * 20.0
-        # angleDegrees = task.time * 20.0
-        # angleRadians = angleDegrees * (math.pi / 180.0)
-        # self.camera.setPos(self.cameraRadius * math.sin(angleRadians), -self.cameraRadius * math.cos(angleRadians), 10)
+        angleDegrees = 1  # task.time * 20.0
+        angleDegrees = task.time * 20.0
+        angleRadians = angleDegrees * (math.pi / 180.0)
+        self.camera.setPos(self.cameraRadius * math.sin(angleRadians), -self.cameraRadius * math.cos(angleRadians), 10)
         self.camera.lookAt(0.0, 0.0, 0.0)
+
         return Task.cont
 
     def update_ode(self, task):
@@ -193,7 +243,7 @@ class Game(ShowBase):
         body = player.getPythonTag("body")
         pos = body.getPosition()
         dt = globalClock.getDt()
-        pos.y += self.speed/2 * dt #divided by two for easier testing
+        # pos.y += self.speed/2 * dt #divided by two for easier testing
 
         if self.keymap["left"]:
             pos.x -= self.speed * dt
